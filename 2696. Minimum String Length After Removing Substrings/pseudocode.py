122e93bfc57739d1c30e0b7242423968ef6f4cff
Winry <86872872+winry-fruitiwi@users.noreply.github.com>
2023-05-27 16:00:26 -0700

    Create pseudocode.py for problem 2696

---
+++ 2696. Minimum String Length After Removing Substrings/pseudocode.py
@@ -0,0 +1,16 @@
+class Solution:
+    # takes in a string of all uppercase letters and removes any instances
+    # of the substrings "AB" or "CD", then returns the length.
+    def minLength(self, s: str) -> int:
+        # iterate over the string, but stop one index before it ends so
+        # that we can subtract any substrings found
+        
+            # if the current character is A, check if the next one is B
+            # if it is, remove the character and reset the iterator
+                # is that possible?
+            
+            # repeat, but with the first character being C and the second D
+        
+        # return the length of the string
+        
+        return 0
