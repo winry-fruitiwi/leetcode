61058e4484f4491850f62d3febb287868eaf8272
Winry <86872872+winry-fruitiwi@users.noreply.github.com>
2023-06-02 11:53:44 -0700

    Update solution.py, complete problem

--- 2696. Minimum String Length After Removing Substrings/solution.py
+++ 2696. Minimum String Length After Removing Substrings/solution.py
@@ -9,19 +9,19 @@ class Solution:
         
         # iterate over the string, but stop one index before it ends so
         # that we can subtract any substrings found
-        while (i < len(s) - 2):
+        while (i < len(s) - 1):
             # if the current character is A, check if the next one is B
             # if it is, remove the character and reset the iterator
                 # is that possible?
             if s[i] == "A" and s[i+1] == "B":
                 s = s[:i] + s[i+2:]
-                i -= 1
+                i = 0
                 continue
             
             # repeat, but with the first character being C and the second D
             if s[i] == "C" and s[i+1] == "D":
                 s = s[:i] + s[i+2:]
-                i -= 1
+                i = 0
                 continue
             
             i += 1
@@ -29,3 +29,4 @@ class Solution:
         # return the length of the string
         
         return len(s)
+    
