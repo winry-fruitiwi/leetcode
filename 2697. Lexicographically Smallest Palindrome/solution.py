aed1a6a51e193d72d9ca62a5b884d8b17c890ec2
Winry <86872872+winry-fruitiwi@users.noreply.github.com>
2023-06-03 13:14:49 -0700

    Create solution.py

---
+++ 2697. Lexicographically Smallest Palindrome/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    # takes in the string s and turns it into a palindrome
+    # by replacing as few letters as possible. breaks tie by
+    # checking the lexicographical value of the current letter
+    def makeSmallestPalindrome(self, s: str) -> str:
+        return ""
