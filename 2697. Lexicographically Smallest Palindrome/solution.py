8d27c1a3d99ffb574c8bbb2790b678aaa07cc590
Winry <86872872+winry-fruitiwi@users.noreply.github.com>
2023-06-07 12:00:03 -0700

    Add pseudocode for 2697

--- 2697. Lexicographically Smallest Palindrome/solution.py
+++ 2697. Lexicographically Smallest Palindrome/solution.py
@@ -1,6 +1,20 @@
 class Solution:
     # takes in the string s and turns it into a palindrome
     # by replacing as few letters as possible. breaks tie by
-    # checking the lexicographical value of the current letter
+    # checking the lexicographical value of the current letter.
+    # returns the palindrome string.
     def makeSmallestPalindrome(self, s: str) -> str:
+        # turn the string into a list for easier manipulation
+        
+        # iterate through the string starting from 0 and ending at half the length (rounded down)
+        # the reason why we're iterating to half the length is because we already
+        # know what the other half of the characters we are iterating over are
+        
+            # get the letter at i
+            
+            # if it's the same as the letter at -i, then compare ords
+            # the letter with the greater ord becomes the letter with the smaller ord
+        
+        # return the new string in string form
+        
         return ""
