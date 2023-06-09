f044c6cf7c1b5f5ef27799b9f2c639e6c09c878e
Winry <86872872+winry-fruitiwi@users.noreply.github.com>
2023-06-09 05:23:50 -0700

    Update solution.py with complete solution
    204 ms
    16.7 mb

--- 2697. Lexicographically Smallest Palindrome/solution.py
+++ 2697. Lexicographically Smallest Palindrome/solution.py
@@ -1,20 +1,33 @@
 class Solution:
-    # takes in the string s and turns it into a palindrome
-    # by replacing as few letters as possible. breaks tie by
+    # Takes in the string s and turns it into a palindrome
+    # by replacing as few letters as possible. Breaks tie by
     # checking the lexicographical value of the current letter.
-    # returns the palindrome string.
+    # Returns the palindrome string.
     def makeSmallestPalindrome(self, s: str) -> str:
-        # turn the string into a list for easier manipulation
+        # turn the string into a list for easier manipulation. This allows
+        # for changing the string's indices directly and I believe it is more
+        # memory efficient.
+        s = list(s)
         
         # iterate through the string starting from 0 and ending at half the length (rounded down)
         # the reason why we're iterating to half the length is because we already
         # know what the other half of the characters we are iterating over are
-        
+        for i in range(int(len(s)/2)):
             # get the letter at i
+            letter = s[i]
             
-            # if it's the same as the letter at -i, then compare ords
+            # if it's not the same as the letter at -i, then compare ords
             # the letter with the greater ord becomes the letter with the smaller ord
+            # however, to compare ords you can just see if the letter is greater or
+            # less than the other
+            if letter > s[-i-1]:
+                s[i] = s[-i-1]
+            elif letter < s[-i-1]:
+                s[-i-1] = s[i]
         
         # return the new string in string form
+        result = ""
+        for e in s:
+            result += e
         
-        return ""
+        return result
