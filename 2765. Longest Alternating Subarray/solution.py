99bd0e3e26a7a08803eccf3bcbad9ac97b831b3d
Winry <86872872+winry-fruitiwi@users.noreply.github.com>
2023-08-06 05:23:08 -0700

    Update solution.py

--- 2765. Longest Alternating Subarray/solution.py
+++ 2765. Longest Alternating Subarray/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     # Takes in an array of numbers and returns the length of the longest
     # alternating subarray, where an alternating subarray is a subarray
-    # with its elements alternating.
+    # with its elements alternating between alternating[0] and alternating[0]+1.
     #
     # Note: The other thing I could do in the pseudocode is to keep a
     # list of all the current alternating subarrays
@@ -10,17 +10,51 @@ class Solution:
     # finding other alternating subarrays. This might be more efficient
     def alternatingSubarray(self, nums: List[int]) -> int:
         # longest subarray length
+        longestAlternating = -1
+        
+        # current subarray length
+        currentSubLen = 0
+        
+        diff = 1
         
         # iterate through nums to the second-to-last element
+        for i in range(len(nums) - 1):
+            # print("i:", i)
+            
+            # retrieve the current and next elements
+            current = nums[i]
+            next = nums[i+1]
+            
+            # if the next element does not satisfy the condition for the new subarray:
+            if next != current + diff:
+                if longestAlternating < currentSubLen:
+                    longestAlternating = currentSubLen
+                
+                currentSubLen = 0
+                
+                # print("no alternating sub:", current, next)
+            
+            # if there is not currently an alternating subarray:
+            if currentSubLen == 0 and next == current + 1:
+                currentSubLen = 2 # because there have been two elements already
+                diff = -1
+                
+                # print("alternating sub found:", current, next)
+            
+            # now, if the next element is current + 1, then the list is still alternating
+            elif next == current + diff:
+                currentSubLen += 1
+                diff *= -1
+                # print("continuing alternating sub:", current, next)
+            
+            # at end of nums or alternation break, update longest subarray length
         
-            # if the next element is 1 greater than the previous one, start
-            # keeping track of the current subarray length and iterate until
-            # the end of the array
-            
-            # make sure that the array is alternating from there
-            
-            # at the end of the loop (end of nums or alternation is broken),
-            # update longest subarray length
+        if longestAlternating < currentSubLen:
+            longestAlternating = currentSubLen
+        
+        if longestAlternating == 0:
+            longestAlternating = -1
         
         # return the longest subarray length
+        return longestAlternating
         
