b0f323ccaa257d03e1be858070364c8311a57b58
Winry <86872872+winry-fruitiwi@users.noreply.github.com>
2023-08-28 13:11:39 -0700

    Create solution.py

---
+++ 2833. Furthest Point From Origin/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        # iterate through commands
+            # use switch-case statement to check for each command
+            
+            # case R: increment
+            
+            # case L: decrement
+            
+            # case _: use negativity of current position to determine which direction to move
+        
+        # return absolute value of current position
