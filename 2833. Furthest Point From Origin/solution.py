eada00601e8be30e6aa199d77f0231d8a3232303
Winry <86872872+winry-fruitiwi@users.noreply.github.com>
2023-09-01 13:12:18 -0700

    Complete implementation

--- 2833. Furthest Point From Origin/solution.py
+++ 2833. Furthest Point From Origin/solution.py
@@ -1,19 +1,30 @@
 class Solution:
     def furthestDistanceFromOrigin(self, moves: str) -> int:
         # number of _ moves
+        numUnknownMoves = 0
+        currentPos = 0
         
         # iterate through commands
-        
+        for command in moves:
             # use match-case statement to check for each command
+            match command:
+                # case R: increment
+                case "R":
+                    currentPos += 1
+                
+                # case L: decrement
+                case "L":
+                    currentPos -= 1
             
-            # case R: increment
-            
-            # case L: decrement
-            
-            # case _: increment number of _ moves
+                # case _: increment number of _ moves
+                case "_":
+                    numUnknownMoves += 1
         
         # check sign of current position
-        
-        # add number of _ moves multiplied by current sign
+        if abs(currentPos) == currentPos:
+            currentPos += numUnknownMoves
+        else:
+            currentPos -= numUnknownMoves
         
         # return absolute value of current position
+        return abs(currentPos)
